baacd3422c212c52ff8187aaf92a5e785cb51791
missionbit <carter117aa@gmail.com>
2018-07-05 09:20:43 -0700

    Added print statement

--- core/src/com/missionbit/character.java
+++ core/src/com/missionbit/character.java
@@ -2,34 +2,39 @@ package com.missionbit;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
+import javax.sound.sampled.LineUnavailableException;
+
+
+public class character extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("");
+		System.out.println("Welcome");
+		}
 
-		System.out.println("Hellow World");
-	}
-
-	@Override
-	public void render () {
+@Override
+public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
+		}
+
+@Override
+public void dispose () {
 		batch.dispose();
 		img.dispose();
-	}
-}
+		}
+		}
