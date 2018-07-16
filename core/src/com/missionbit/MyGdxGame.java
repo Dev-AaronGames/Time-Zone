6002016396296d688cbb95502ab7a6e156ba3e39
missionbit <carter117aa@gmail.com>
2018-07-16 12:05:08 -0700

    Added print statement

--- core/src/com/missionbit/MyGdxGame.java
+++ core/src/com/missionbit/MyGdxGame.java
@@ -3,33 +3,88 @@ package com.missionbit;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.missionbit.Actors.Controller;
+import com.missionbit.Actors.Player;
 
 public class MyGdxGame extends ApplicationAdapter {
+
+
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 480;
+	public static final String TITLE = "LibGDX Sample";
+	private Controller controller;
+	OrthographicCamera camera;
+
+
 	SpriteBatch batch;
 	Texture img;
-	
+	Player ghost;
+ShapeRenderer sr;
+	Texture Background;
+
+
 	@Override
 	public void create () {
+sr = new ShapeRenderer();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,MyGdxGame.WIDTH,MyGdxGame.HEIGHT);
+		controller = new Controller(camera);
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-
+	ghost = new Player(100,100,500);
 		System.out.println("Hello World!!!! - Make sure to update every time you make/complete a task.");
+
+
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		ghost.update(Gdx.graphics.getDeltaTime());
+
+
+		if (controller.isLeftPressed()){
+
+			ghost.moveLeft();
+		}else if(controller.isRightPressed()){
+
+			ghost.moveRight();
+
+		} else {
+			ghost.resetAnim();
+
+		}
+
+		if (controller.isJumpPressed()){
+			System.out.println("hi22");
+			ghost.jump();
+		}
+
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		controller.draw(batch);
+		ghost.draw(batch);
+
 		batch.end();
+
+sr.begin(ShapeRenderer.ShapeType.Line);
+
+controller.drawDebug(sr);
+sr.end();
+
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+
 	}
 }
--- desktop/src/com/missionbit/desktop/DesktopLauncher.java
+++ desktop/src/com/missionbit/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.missionbit.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = MyGdxGame.WIDTH;
+		config.height = MyGdxGame.HEIGHT;
+		config.title = MyGdxGame.TITLE;
+
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
