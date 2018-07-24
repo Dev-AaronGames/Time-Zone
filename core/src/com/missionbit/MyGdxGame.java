f9c833208a8c5c92aeeb4b0cb42998e5ffc28ce7
missionbit <carter117aa@gmail.com>
2018-07-24 10:47:05 -0700

    fix conflict

--- core/src/com/missionbit/MyGdxGame.java
+++ core/src/com/missionbit/MyGdxGame.java
@@ -7,8 +7,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.missionbit.Actors.Controller;
-import com.missionbit.Actors.Player;
+import com.missionbit.States.PlayState;
 
 public class MyGdxGame extends Game {
 
@@ -16,14 +15,14 @@ public class MyGdxGame extends Game {
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 480;
 	public static final String TITLE = "LibGDX Sample";
-	private Controller controller;
+
 	OrthographicCamera camera;
 
 
 	public SpriteBatch batch;
 
-	Player ghost;
-	ShapeRenderer sr;
+
+	public ShapeRenderer sr;
 	Texture Background;
 
 
@@ -32,11 +31,11 @@ public class MyGdxGame extends Game {
 	public void create () {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false,MyGdxGame.WIDTH,MyGdxGame.HEIGHT);
-		ghost = new Player(100,100,100);
-		controller = new Controller(camera);
+
 		Background = new Texture("landscape.png");
 		batch = new SpriteBatch();
 		sr = new ShapeRenderer();
+		setScreen(new PlayState(this));
 
 	}
 
@@ -46,46 +45,6 @@ public class MyGdxGame extends Game {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		super.render();
 		Gdx.gl.glClear(GL20.GL_STENCIL_VALUE_MASK);
-		if (controller.isLeftPressed()){
-
-			ghost.moveLeft();
-		}else if(controller.isRightPressed()){
-
-			ghost.moveRight();
-
-		} else {
-			ghost.resetAnim();
-
-		}
-
-		if (controller.isJumpPressed()){
-			ghost.jump();
-		}
-
-		if (controller.isAttackPressed()){
-			ghost.atack();
-		}
-		if (controller.isFirePressed()){
-			ghost.fire();
-		}
-
-		ghost.update(Gdx.graphics.getDeltaTime());
-
-		batch.begin();
-		batch.draw(
-				ghost.getTexture(Gdx.graphics.getDeltaTime()),
-				ghost.getPosition().x,
-				ghost.getPosition().y,
-				250,
-				250);
-		controller.draw(batch);
-		batch.end();
-
-
-
-		sr.begin(ShapeRenderer.ShapeType.Line);
-		controller.drawDebug(sr);
-		sr.end();
 
 	}
 
